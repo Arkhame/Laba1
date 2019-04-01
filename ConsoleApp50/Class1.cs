1cb3d3cd13b0c0944a02f1667a8f54a0a8998c7e
Arkhame <alesundwq@gmail.com>
2019-04-01 16:48:15 +0300

    Kaha1

---
+++ ConsoleApp50/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите данные");
+            Product kaha = new Product();
+            kaha.SetInfo();
+            kaha.GetInfo();
+            Console.ReadKey();
+        }
+    }
+}
